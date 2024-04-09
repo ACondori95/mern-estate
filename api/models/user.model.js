6ed5efd98b1350480229e1c230d555404572e0b2
Alvaro Condori <con21033@byui.edu>
2024-04-08 21:04:25 -0300

    create and add OAuth component

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    avatar: {
+      type: String,
+      default:
+        "https://imgs.search.brave.com/vimRBqV1gbtSp6lSKHUfYpGmtB4YmLo41a2LhLldkS4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzRkLzJl/LzBhLzRkMmUwYTY5/NDAxNWYzZDJmODQw/ODczZDAxYWE1ZmQ0/LmpwZw",
+    },
   },
   {timestamps: true}
 );
